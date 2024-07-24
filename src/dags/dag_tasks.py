4101288538268cbfefdc614259030486fbdec65e
erickAccenture <erickperezargueta2@gmail.com>
2024-07-24 10:52:19 -0600

    agrega carpeta imports train

--- src/dags/dag_tasks.py
+++ src/dags/dag_tasks.py
@@ -13,8 +13,8 @@ from snowflake.core._common import CreateMode
 from snowflake.core.task import StoredProcedureCall
 from snowflake.core.task.dagv1 import DAG, DAGTask, DAGOperation
 
-from process_func import process_data
-from train_func import train_register
+from imports_train_pipeline.process_func import process_data
+from imports_train_pipeline.train_func import train_register
 
 
 my_dir = os.path.dirname(os.path.realpath(__file__))
@@ -58,7 +58,7 @@ with DAG("DAG_TRAIN") as dag_train:
             func=process_data,
             stage_location=f"@{dict_creds['database']}.{dict_creds['schema']}.ML_MODELS/TRAIN_PIPELINE/PROCESS",
             packages=['snowflake-ml-python', 'snowflake-snowpark-python'],
-            imports=[os.path.join(my_dir, 'process_func.py')]
+            imports=['dags\imports_train_pipeline']
         ),
         warehouse="COMPUTE_WH"
     )
@@ -68,7 +68,7 @@ with DAG("DAG_TRAIN") as dag_train:
             func=train_register,
             stage_location=f"@{dict_creds['database']}.{dict_creds['schema']}.ML_MODELS/TRAIN_PIPELINE/TRAIN",
             packages=['snowflake-ml-python', 'snowflake-snowpark-python'],
-            imports=[os.path.join(my_dir, 'train_func.py')]
+            imports=['dags\imports_train_pipeline']
         ),
         warehouse="COMPUTE_WH"
     )
@@ -76,29 +76,29 @@ with DAG("DAG_TRAIN") as dag_train:
 dag_task1_train >> dag_task2_train
 
 
-with DAG("DAG_INFERENCE") as dag_inference:
-    dag_task1_inference = DAGTask(
-        "process",
-        StoredProcedureCall(
-            func=process_data,
-            stage_location=f"@{dict_creds['database']}.{dict_creds['schema']}.ML_MODELS/INFERENCE_PIPELINE/PROCESS",
-            packages=['snowflake-ml-python', 'snowflake-snowpark-python'],
-            imports=[os.path.join(my_dir, 'process_func.py')]
-        ),
-        warehouse="COMPUTE_WH"
-    )
-    dag_task2_inference = DAGTask(
-        "train_register",
-        StoredProcedureCall(
-            func=train_register,
-            stage_location=f"@{dict_creds['database']}.{dict_creds['schema']}.ML_MODELS/INFERENCE_PIPELINE/INFERENCE",
-            packages=['snowflake-ml-python', 'snowflake-snowpark-python'],
-            imports=[os.path.join(my_dir, 'train_func.py')]
-        ),
-        warehouse="COMPUTE_WH"
-    )
+# with DAG("DAG_INFERENCE") as dag_inference:
+#     dag_task1_inference = DAGTask(
+#         "process",
+#         StoredProcedureCall(
+#             func=process_data,
+#             stage_location=f"@{dict_creds['database']}.{dict_creds['schema']}.ML_MODELS/INFERENCE_PIPELINE/PROCESS",
+#             packages=['snowflake-ml-python', 'snowflake-snowpark-python'],
+#             imports=[os.path.join(my_dir, 'process_func.py')]
+#         ),
+#         warehouse="COMPUTE_WH"
+#     )
+#     dag_task2_inference = DAGTask(
+#         "train_register",
+#         StoredProcedureCall(
+#             func=train_register,
+#             stage_location=f"@{dict_creds['database']}.{dict_creds['schema']}.ML_MODELS/INFERENCE_PIPELINE/INFERENCE",
+#             packages=['snowflake-ml-python', 'snowflake-snowpark-python'],
+#             imports=[os.path.join(my_dir, 'train_func.py')]
+#         ),
+#         warehouse="COMPUTE_WH"
+#     )
 
-dag_task1_inference >> dag_task2_inference
+# dag_task1_inference >> dag_task2_inference
 
 
 
@@ -109,7 +109,7 @@ dag_op_train.deploy(dag_train, CreateMode.or_replace)
 dag_op_train.run(dag_train)
 
 
-root_inference = Root(session)
-schema_inference = root_inference.databases[dict_creds['database']].schemas[dict_creds['schema']]
-dag_op_inference = DAGOperation(schema_inference)
-dag_op_inference.deploy(dag_inference, CreateMode.or_replace)
+# root_inference = Root(session)
+# schema_inference = root_inference.databases[dict_creds['database']].schemas[dict_creds['schema']]
+# dag_op_inference = DAGOperation(schema_inference)
+# dag_op_inference.deploy(dag_inference, CreateMode.or_replace)
